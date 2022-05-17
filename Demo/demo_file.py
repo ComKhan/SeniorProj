54e5f400b31ec35c4f4a337350410f8bd6b0127d
ComKhan <54348367+ComKhan@users.noreply.github.com>
2022-05-16 23:15:16 -0700

    small update to demo_file

--- Demo/demo_file.py
+++ Demo/demo_file.py
@@ -4,7 +4,6 @@ import numpy as np
 from scipy.fft import fft, ifft
 from scipy.signal import hilbert
 import wave
-import np2
 import time
 from fir_filter import fir_filt
 import wave
@@ -80,39 +79,39 @@ def process_data(process_val, chunk_process, fs):
 
 
 # set all base requirements for class creation
-form_1 = pyaudio.paInt16
-chans = 1
-samp_rate = 48000
-chunk = 4096
-record_secs = 1
-dev_index = 1  # this is the device index (usb device specific port) connection in hardware VARIES
-wav_output_filename = 'test5.wav'
-audio = pyaudio.PyAudio()
+# form_1 = pyaudio.paInt16
+# chans = 1
+# samp_rate = 48000
+# chunk = 4096
+# record_secs = 1
+# dev_index = 1  # this is the device index (usb device specific port) connection in hardware VARIES
+# wav_output_filename = 'test5.wav'
+# audio = pyaudio.PyAudio()
 
-stream = audio.open(format=form_1, rate=samp_rate, channels=chans, \
-                    input_device_index=dev_index, input=True, \
-                    frames_per_buffer=chunk)
+# stream = audio.open(format=form_1, rate=samp_rate, channels=chans, \
+#                     input_device_index=dev_index, input=True, \
+#                     frames_per_buffer=chunk)
 
-print("recording")
+# print("recording")
 
-frames = []
+# frames = []
 
-stream.start_stream()
-for i in range(0, int((samp_rate / chunk) * record_secs)):
-    data = stream.read(chunk, exception_on_overflow=False)
-    frames.append(data)
+# stream.start_stream()
+# for i in range(0, int((samp_rate / chunk) * record_secs)):
+#     data = stream.read(chunk, exception_on_overflow=False)
+#     frames.append(data)
 
-print("finished recording")
-stream.stop_stream()
-stream.close()
-audio.terminate()
+# print("finished recording")
+# stream.stop_stream()
+# stream.close()
+# audio.terminate()
 
-wavefile = wave.open(wav_output_filename, 'wb')
-wavefile.setnchannels(chans)
-wavefile.setsampwidth(audio.get_sample_size(form_1))
-wavefile.setframerate(samp_rate)
-wavefile.writeframes(b''.join(frames))
-wavefile.close()
+# wavefile = wave.open(wav_output_filename, 'wb')
+# wavefile.setnchannels(chans)
+# wavefile.setsampwidth(audio.get_sample_size(form_1))
+# wavefile.setframerate(samp_rate)
+# wavefile.writeframes(b''.join(frames))
+# wavefile.close()
 
 
 
