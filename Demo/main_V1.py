4c6af2b9821a5d67b82e1835b79d6ded552105b0
evkoush <59993428+evkoush@users.noreply.github.com>
2022-05-17 10:22:05 -0700

    demo for fsm

---
+++ Demo/main_V1.py
@@ -0,0 +1,208 @@
+'''
+Senior Project
+Humming Bluetooth Synthesizer
+main.py
+'''
+# imports for buttons.py
+import RPi.GPIO as GPIO
+from gpiozero import Button
+
+
+from multiprocessing.pool import INIT
+#import buttons
+import time
+
+'''
+# imports for usb audio
+import pyaudio
+import matplotlib.pyplot as plt
+import numpy as np
+import wave
+'''
+
+# Import local py files
+import rootFSM as fsm
+import btns_V1
+
+'''
+global variables
+
+
+'''
+
+''' Using fsm code
+States
+
+'''
+
+
+class currProps(object): # stores current values for FSM
+    def __init__(self):
+        self.currState = INIT
+
+
+current = currProps()
+
+##================================================================================
+
+#recordVal = buttons.btnClk() # instance of settings for the record button
+#recordVal.clicked()
+
+
+# method in video, using Char method
+inFSM = True # stays in FSM while true
+#global btn1, btn2, btn3, btn4, btn5, btn6, btn7
+
+if __name__ == "__main__":
+    mainFSM = fsm.Char() # create an instance of the FSM
+    
+    # add states instances
+    mainFSM.FSM.states["InitS"] = fsm.InitS() #instance of Set state stored within state dictionary inside FSM
+    mainFSM.FSM.states["WaitS"] = fsm.WaitS()
+    mainFSM.FSM.states["FilterS"] = fsm.FilterS()
+    mainFSM.FSM.states["InstS"] = fsm.InstS()
+    mainFSM.FSM.states["OutputS"] = fsm.OutputS()
+    mainFSM.FSM.states["RecordS"] = fsm.RecordS()
+    mainFSM.FSM.states["StoreS"] = fsm.StoreS()
+    mainFSM.FSM.states["PlayS"] = fsm.PlayS()
+    mainFSM.FSM.states["QuickVolS"] = fsm.QuickVolS()
+    mainFSM.FSM.states["SetVolS"] = fsm.SetVolS()
+    mainFSM.FSM.transitions["toWaitS"] = fsm.Transition("WaitS") # create instance of those transitions and store them within trans dictionary
+    mainFSM.FSM.transitions["toInitS"] = fsm.Transition("InitS")
+    mainFSM.FSM.transitions["toFilterS"] = fsm.Transition("FilterS")
+    mainFSM.FSM.transitions["toInstS"] = fsm.Transition("InstS")
+    mainFSM.FSM.transitions["toOutputS"] = fsm.Transition("OutputS")
+    mainFSM.FSM.transitions["toRecordS"] = fsm.Transition("RecordS")
+    mainFSM.FSM.transitions["toStoreS"] = fsm.Transition("StoreS")
+    mainFSM.FSM.transitions["toPlayS"] = fsm.Transition("PlayS")
+    mainFSM.FSM.transitions["toQuickVolS"] = fsm.Transition("QuickVolS")
+    mainFSM.FSM.transitions["toSetVolS"] = fsm.Transition("SetVolS")
+    
+    #set initial state
+    mainFSM.FSM.SetState("InitS")
+    
+    print("enter main now")
+    while inFSM:
+        mainFSM.FSM.Go()
+        testName = mainFSM.FSM.curStateName
+        testTrans = mainFSM.FSM.trans
+        
+        if testTrans == None: # run and check for transitions
+            #print("trans == None")
+            #mainFSM.FSM.Go()
+            if (testName == "WaitS") | (testName == "InitS"):
+                if btns_V1.clickA.implement == False: # set up for each button
+                    mainFSM.FSM.Transition("toFilterS")
+                    testTrans = mainFSM.FSM.trans
+                    btns_V1.clickA.implement = True
+
+                if btns_V1.clickB.implement == False: # set up for each button
+                    mainFSM.FSM.Transition("toInstS")
+                    testTrans = mainFSM.FSM.trans
+                    btns_V1.clickB.implement = True
+
+                if btns_V1.clickC.implement == False: # set up for each button
+                    #mainFSM.FSM.Transition("toInitS")
+                    #testTrans = mainFSM.FSM.trans
+                    print("no use for btn in this state")
+                    btns_V1.clickC.implement = True
+
+                if btns_V1.clickD.implement == False: # set up for each button
+                    mainFSM.FSM.Transition("toOutputS")
+                    testTrans = mainFSM.FSM.trans
+                    btns_V1.clickD.implement = True
+
+                if btns_V1.clickE.implement == False: # set up for each button
+                    mainFSM.FSM.Transition("toRecordS")
+                    testTrans = mainFSM.FSM.trans
+                    btns_V1.clickD.implement = True
+
+                if btns_V1.clickF.implement == False: # set up for each button
+                    mainFSM.FSM.Transition("toSetVolS")
+                    testTrans = mainFSM.FSM.trans
+                    btns_V1.clickF.implement = True
+
+                if btns_V1.clickG.implement == False: # set up for each button
+                    mainFSM.FSM.Transition("toQuickVolS")
+                    testTrans = mainFSM.FSM.trans
+                    btns_V1.clickG.implement = True
+
+            if ((testName == "SetVolS") | (testName == "FilterS") | (testName == "InstS") | 
+                (testName == "QuickVolS") | (testName == "SetVolS") | (testName == "OutputS")):
+                mainFSM.FSM.Transition("toWaitS")
+                testTrans = mainFSM.FSM.trans
+                
+            if (testName == "RecordS"): # stuck on: button doesn't reset to false
+                if btns_V1.clickE.implement == False:
+                    mainFSM.FSM.Transition("toStoreS")
+                    testTrans = mainFSM.FSM.trans
+                    btns_V1.clickE.implement = True
+
+            if (testName == "StoreS"):
+                if btns_V1.clickC.implement == False:
+                    mainFSM.FSM.Transition("toPlayS")
+                    testTrans = mainFSM.FSM.trans
+                    btns_V1.clickC.implement = True
+
+                if btns_V1.clickD.implement == False:
+                    mainFSM.FSM.Transition("toWaitS")
+                    testTrans = mainFSM.FSM.trans
+                    btns_V1.clickD.implement = True
+
+            if (testName == "PlayS"):
+                if btns_V1.clickC.implement == False:
+                    mainFSM.FSM.Transition("toPlayS")
+                    testTrans = mainFSM.FSM.trans
+                    btns_V1.clickC.implement = True
+
+                if btns_V1.clickD.implement == False:
+                    mainFSM.FSM.Transition("toWaitS")
+                    testTrans = mainFSM.FSM.trans
+                    btns_V1.clickD.implement = True
+
+        else: # implement transition and run
+            #print("edge case FAIL")
+            mainFSM.FSM.Go()
+            mainFSM.FSM.Transition(mainFSM.FSM.transName)
+            
+
+        
+        pass
+    
+## working fsm but messy, make sure to add Go() after all cases
+'''if testName == "InitS": 
+            btn1.when_pressed = btns_V1.clickA.clicked
+            if btn1.is_pressed:
+                btns_V1.clickA.clicked()
+                print("state A clicked button")
+            if btns_V1.clickA.implement == False:
+                print("state A clicked button")
+                mainFSM.FSM.Transition("toWaitS")
+                btns_V1.clickA.implement = True
+            # Testing code: Remove for full implementation
+            #lcd.write_lcd("Synth Start    F", "A B C D E      G") # add when import lcd
+            #print("Synth Start    F")
+            #mainFSM.FSM.Transition("toWaitS")
+            #time.sleep(5)
+            # End Testing Code
+
+            pass
+
+        elif testName == "WaitS": 
+            # button presses will cause transitions to other states
+            #btn1.when_pressed = btns_V1.clickA.clicked
+            btns_V1.btn2.when_pressed = btns_V1.clickB.clicked()
+            btns_V1.btn3.when_pressed = btns_V1.clickC.clicked()
+            btns_V1.btn4.when_pressed = btns_V1.clickD.clicked()
+            btns_V1.btn5.when_pressed = btns_V1.clickE.clicked()
+            btns_V1.btn6.when_pressed = btns_V1.clickF.clicked()
+            btns_V1.btn7.when_pressed = btns_V1.clickG.clicked()
+
+            # Testing code: Remove for full implementation
+            #mainFSM.FSM.Transition("toInitS")
+            #mainFSM.FSM.curStateName = "InitS"
+            print("State 2")
+            time.sleep(5)
+            # End Testing Code
+
+            pass'''
---
+++ Demo/rootFSM.py
@@ -0,0 +1,380 @@
+# https://www.youtube.com/watch?v=E45v2dD3IQU&ab_channel=TrevorPayne
+from operator import truediv
+from tkinter import NONE
+from types import prepare_class
+from random import randint
+import time
+import btns_V1
+# imports for buttons.py
+import RPi.GPIO as GPIO
+from gpiozero import Button
+import main_V1
+#import lcd
+
+#import auRec as au
+
+State = type("States", (object,), {})
+btn1 = Button(16) #Filter, Yes
+btn2 = Button(6) # Inst
+btn3 = Button(5) # No
+btn4 = Button(0) # Output
+btn5 = Button(4) # Record
+btn6 = Button(3) # Vol Dwn
+btn7 = Button(2) # Vol Up
+
+'''#Alberto's code
+import multiprocessing
+import pyaudio
+import numpy as np
+from scipy.fft import fft,ifft
+from scipy.signal import hilbert
+import wave
+import np2
+import time
+from fir_filter import fir_filt
+import wave
+
+form_1 = pyaudio.paInt16
+chans = 1
+samp_rate = 48000
+chunk =4096
+record_secs = 5
+dev_index = 1   #this is the device index (usb device specific port) connection in hardware VARIES
+#wav_output_filename = 'test6.wav'
+audio = pyaudio.PyAudio()
+
+stream = audio.open(format = form_1, rate = samp_rate, channels = chans, \
+                    input_device_index = dev_index, input = True, \
+                    frames_per_buffer = chunk)
+data= multiprocessing.Value('i')
+data.value= 0
+
+queue_count = multiprocessing.Value('i')
+queue_count.value = 0
+
+final_freq = multiprocessing.Array('f',4096)
+
+final_vol= multiprocessing.Array('i',4096)'''
+
+
+class InitS(State):
+    def __init__(self):
+        
+        print("Init InitS")
+        pass
+
+    def Go(self):
+        #btns_V1.initialize()
+        print("go InitS")
+        time.sleep(2)
+        btn1.when_pressed = btns_V1.clickA.clicked
+        btn2.when_pressed = btns_V1.clickB.clicked
+        btn3.when_pressed = btns_V1.clickC.clicked
+        btn4.when_pressed = btns_V1.clickD.clicked
+        btn5.when_pressed = btns_V1.clickE.clicked
+        btn6.when_pressed = btns_V1.clickF.clicked
+        btn7.when_pressed = btns_V1.clickG.clicked
+        '''if btn1.is_pressed: # wait  for button clicks within states
+            btns_V1.clickA.clicked()
+            print("state A clicked button")'''
+        '''if btns_V1.clickA.implement == False: # check for button click updates in main, bc transitions should be triggered there
+            print("state A clicked button")
+            mainFSM.FSM.Transition("toWaitS")
+            btns_V1.clickA.implement = True'''
+        pass
+    
+
+class WaitS(State):
+    def __init__(self):
+        # update variable corresponding to the button
+        
+        pass
+
+    def Go(self):
+        #print("go WaitS")
+        btn1.when_pressed = btns_V1.clickA.clicked
+        btn2.when_pressed = btns_V1.clickB.clicked
+        btn3.when_pressed = btns_V1.clickC.clicked
+        btn4.when_pressed = btns_V1.clickD.clicked
+        btn5.when_pressed = btns_V1.clickE.clicked
+        btn6.when_pressed = btns_V1.clickF.clicked
+        btn7.when_pressed = btns_V1.clickG.clicked
+
+        # Testing code: Remove for full implementation
+        #mainFSM.FSM.Transition("toInitS")
+        #mainFSM.FSM.curStateName = "InitS"
+        
+        pass
+
+
+class RecordS(State):
+    def __init__(self):
+        # update variable corresponding to the button
+        
+        pass
+
+    def Go(self):
+        btn5.when_pressed = btns_V1.clickE.clicked
+        
+        pass
+
+class StoreS(State):
+    def __init__(self):
+        # update variable corresponding to the button
+        
+        pass
+
+    def Go(self):
+        btn3.when_pressed = btns_V1.clickC.clicked
+        btn4.when_pressed = btns_V1.clickD.clicked
+        '''q = multiprocessing.Queue()
+        p = multiprocessing.Process(target = au.get_data,args = (stream,samp_rate,chunk,q,data,audio))
+        p1 = multiprocessing.Process(target = au.get_freq, args = (samp_rate,chunk,q,data,final_freq,final_vol,queue_count))
+    
+        tic = time.time()
+        p.start()
+        p1.start()
+        p.join()
+        p1.join()
+        
+        toc = time.time()
+        print(list(final_freq[:]))
+        print(queue_count.value)
+        print(toc-tic)
+        i = 0'''
+        pass
+
+class PlayS(State):
+    def __init__(self):
+        # update variable corresponding to the button
+        
+        pass
+
+    def Go(self):
+        btn3.when_pressed = btns_V1.clickC.clicked
+        btn4.when_pressed = btns_V1.clickD.clicked
+        pass
+
+class QuickVolS(State):
+    def __init__(self):
+        # update variable corresponding to the button
+        print("Init QuickVolS")
+        pass
+
+    def Go(self):
+        print("go QuickVolS")
+        pass
+
+class SetVolS(State):
+    def __init__(self):
+        # update variable corresponding to the button
+        print("Init SetVolS")
+        pass
+
+    def Go(self):
+        print("go SetVolS")
+        pass
+
+class InstS(State):
+    def __init__(self):
+        # update variable corresponding to the button
+        print("Init SetInstS")
+        pass
+
+    def Go(self):
+        print("go SetInstS")
+        pass
+
+class FilterS(State):
+    def __init__(self):
+        # update variable corresponding to the button
+        print("Init SetFilterS")
+        pass
+
+    def Go(self):
+        print("go SetFilterS")
+        pass
+
+class OutputS(State):
+    def __init__(self):
+        # update variable corresponding to the button
+        print("Init SetOutS")
+        pass
+
+    def Go(self):
+        print("go SetOutS")
+        pass
+
+
+##============================================================================
+
+class Transition():
+    def __init__(self, toState):
+        self.toState = toState
+        #self.curState = toState
+        #self.curStateName = toState
+        print("trans init")
+
+    def Go(self):
+        #self.toState = toState
+        #self.SetState(self.toState)
+        #self.curStateName = self.toState
+        print("trans go")
+
+##============================================================================
+
+class SimpleFSM(object):
+    def __init__(self, char):
+        self.char = char
+        self.states = {}        # state dictionary
+        self.transitions = {}   # transition dictionary
+        self.curState = None    # current state
+        self.curStateName = None
+        self.trans = None       # current transition
+        self.transName = None
+        print("Init simpFSM")
+
+    def SetState(self, stateName): # look at string passed within dictionary
+        self.curState = self.states[stateName]  # paired with a state instance
+        self.curStateName = stateName
+        print("set state simpFSM")
+
+    def Transition(self, transName):  
+        self.trans = self.transitions[transName]
+        self.transName = transName
+        print("trans simpFSM")
+
+    def Go(self):
+        if(self.trans):             # if there's a transition stored within the trans
+            self.trans.Go()    # execute that transition
+            self.SetState(self.trans.toState) # set to transitioned state
+            self.trans = None       # reset transition to None
+            self.transName = None
+            print("in state " + self.curStateName)
+        self.curState.Go()     # execute current state
+        #print("go simpfsm")
+
+##============================================================================ 
+# holds all character attributes and properties
+# FSM is stored within this class
+
+class Char(object):
+    def __init__(self):
+        self.FSM = SimpleFSM(self) # create an instance of the fsm
+        self.Set = True # store propery to true
+        print("char init")
+
+
+##============================================================================
+'''
+if __name__ == "__main__":
+    light = SimpleFSM()
+
+    light.states["Set"] = Set() #instance of Set state stored within state dictionary inside FSM
+    light.states["Off"] = Clicked()
+    light.transitions["toOn"] = Transition("Set") # create instance of those transitions and store them within trans dictionary
+    light.transitions["toOff"] = Transition("Off")
+
+    #set initial state
+    light.SetState("Set")
+
+    for i in range(20):
+        timeInterval = 1
+        
+        if (randint(0,2)):
+            if(light.Set):
+                light.Transition("toOff")
+                #light.Set = False
+            else:
+                light.Transition("toOn")
+                #light.Set = True
+
+        light.Go()
+
+
+'''
+# method in video, using Char method
+'''inFSM = True # stays in FSM while true
+
+if __name__ == "__main__":
+    mainFSM = Char() # create an instance of the FSM
+    
+    # add states instances
+    mainFSM.FSM.states["InitS"] = InitS() #instance of Set state stored within state dictionary inside FSM
+    mainFSM.FSM.states["WaitS"] = WaitS()
+    mainFSM.FSM.transitions["toWaitS"] = Transition("WaitS") # create instance of those transitions and store them within trans dictionary
+    mainFSM.FSM.transitions["toInitS"] = Transition("InitS")
+    
+    #set initial state
+    mainFSM.FSM.SetState("InitS")
+
+    print("enter main now")
+    while inFSM:
+        matchName = mainFSM.FSM.curStateName
+        if matchName == "InitS":
+            #lcd.write_lcd("Synth Start    F", "A B C D E      G") # add when import lcd
+            print("Synth Start    F")
+            mainFSM.FSM.Transition("toWaitS")
+            time.sleep(5)
+            pass
+
+        elif matchName == "WaitS":
+            mainFSM.FSM.Transition("toInitS")
+            mainFSM.FSM.curStateName = "InitS"
+            print("State 2")
+            time.sleep(5)
+            pass
+
+        elif matchName == "RecordS":
+
+            pass
+
+        elif matchName == "StoreS":
+
+            pass
+        
+        elif matchName == "PlayS":
+
+            pass
+
+        elif matchName == "QuickVol":
+
+            pass
+
+        elif matchName == "SetVolS":
+
+            pass
+            
+        elif matchName == "SetInstS":
+
+            pass
+
+        elif matchName == "SetFilterS":
+
+            pass
+
+        elif matchName == "SetOutS":
+
+            pass   
+
+        else:
+            print("edge case: FAIL")
+            pass
+
+        mainFSM.FSM.Go()
+        pass
+    
+    for i in range(20):
+        timeInterval = 1
+        
+        if (randint(0,2)):
+            if(light.Set):
+                light.FSM.Transition("toOff")
+                light.Set = False
+            else:
+                light.FSM.Transition("toOn")
+                light.Set = True
+
+        light.FSM.Go()
+
+    '''
