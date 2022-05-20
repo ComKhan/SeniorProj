9640f0b980e222074bf2f483b41f976ce8978cc1
evkoush <elizavetakoush@gmail.com>
2022-05-19 18:10:41 -0700

    fix button_press.value

--- get_functions.py
+++ get_functions.py
@@ -12,11 +12,11 @@ def get_data(obj, sample_rate1, chunk1, q1, data1, aud, button_press1):
     obj.start_stream()
 
     # set value to count to
-    while button_press1 != 1:
+    while button_press1.value != 1:
         # ADD INTERRUPT HERE
         fsm.btn5.when_pressed = btns.clickE.clicked
         if (btns.clickE.clicked == False):
-            button_press1 = 1
+            button_press1.value = 1
 
         # read data from usb mic
         val = (obj.read(chunk1, exception_on_overflow=False))
@@ -33,7 +33,7 @@ def get_data(obj, sample_rate1, chunk1, q1, data1, aud, button_press1):
         # ADD INTTERUPT HERE AS WELL MAYBE
         fsm.btn5.when_pressed = btns.clickE.clicked
         if (btns.clickE.clicked == False):
-            button_press1 = 1
+            button_press1.value = 1
 
     # setting data1.value to 0 for another condition
     data1.value = 0
