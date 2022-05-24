39e0fe1ff643aa4b48a0a9217e181ae570538467
evkoush <elizavetakoush@gmail.com>
2022-05-23 20:56:55 -0700

    testing multiprocessing with buttons

--- test_mult.py
+++ test_mult.py
@@ -4,40 +4,70 @@ import rootFSM as fsm
 
 def process_1(button_press1):
     i = 0
+    print("running")
+    '''fsm.btn5.when_pressed = btns.clickE.clicked
+    if (btns.clickE.implement == False):
+        button_press1.value = 1
+        print("p1 click")
+        btns.clickE.implement = True
+
 
     while button_press.value != 1:
         fsm.btn5.when_pressed = btns.clickE.clicked
         if (btns.clickE.implement == False):
             button_press1.value = 1
             print("p1 click")
+            btns.clickE.implement = True'''
 
-        print(i)
+    while  button_press1 != 1:
+        if (i%2000 == 0):
+            #print(i)
+            pass
         i = i+1
 
+    '''fsm.btn5.when_pressed = btns.clickE.clicked
+    if (btns.clickE.implement == False):
+        button_press1.value = 1
+        print("p1 click")
+        btns.clickE.implement = True'''
+
+
+    print("done")
 
 
 def process_2(button_press2):
+    #print("p2 go")
+    fsm.btn5.when_pressed = btns.clickE.clicked
+    if (btns.clickE.implement == False):
+        button_press2.value = 1
+        print("p1 click")
+        btns.clickE.implement = True
 
-    i = 0
-    while button_press.value == 0:
+    while button_press.value != 1:
+        #print("yes")
         fsm.btn5.when_pressed = btns.clickE.clicked
         if (btns.clickE.implement == False):
             button_press2.value = 1
-            print("p1 click")
+            print("p2 click")
             btns.clickE.c
-        i = i+1
+            btns.clickE.implement = True
+
+        #i = i+1
 
     if button_press.value == 1:
         print('button works')
 
+    print("done2")
 
 
-if __name__ == "main":
+
+if __name__ == "__main__":
 
     button_press = multiprocessing.Value('i')
     button_press.value = 0
-    p = multiprocessing.Process(target = (process_1), args=(button_press))
-    p1 = multiprocessing.Process(target = (process_2), args = (button_press))
+
+    p = multiprocessing.Process(target = (process_1), args=(button_press,))
+    p1 = multiprocessing.Process(target = (process_2), args = (button_press,))
 
 
     p.start()
