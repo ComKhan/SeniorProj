4f3d71be70f1fd757b1b7c9648b3cc214ec49419
ComKhan <54348367+ComKhan@users.noreply.github.com>
2022-05-24 09:59:35 -0700

    Wavetable is functional

--- Sound_Processing/synth.py
+++ Sound_Processing/synth.py
@@ -6,7 +6,7 @@ import math
 import time
 import wavFuncs
 
-def plotWaveform(sound, time = 0.05):
+def plotWaveform(sound, time = 0.05, Fs = 48000):
     if type(sound) == str:
         Fs, y = read(sound)
         try:
@@ -24,7 +24,7 @@ def plotWaveform(sound, time = 0.05):
         y = sound
 
     T = 1 / Fs
-    N=int(N)
+    N = len(y)
     x = np.linspace(0.0, N * T, N, endpoint=False)
 
     if type(sound) == int:
@@ -41,19 +41,29 @@ def plotWaveform(sound, time = 0.05):
 def interpolate(first, second, dist):
     return first + (dist*(second-first))
 
-def setPer(Amplitude, tfreq, instrument_file, runtime, Fs):
-    FS, y = read(instrument_file)
+def setPer(Amplitude, tfreq, instrument_file, time):
+    Fs, y = read(instrument_file)
     i = 0
     output = []
     length = len(y)
-    while i < Fs*runtime:
-        output.append(Amplitude*y[(i*tfreq//110)%length])
+    sd.play(y, Fs)
+    sd.wait()
+    plotWaveform(y, length/Fs, Fs)
+    while i < Fs*time:
+        output.append(Amplitude*interpolate(y[(i*tfreq//110) % (length-1)],
+                                            y[((i*tfreq//110) % (length-1))+1],
+                                            (i*tfreq//110)%1))
         i += 1
 
     output = np.array(output)
 
-    sd.play(output, FS)
-    return FS, y
+    print(output)
+    write('setPer.wav', Fs, output)
+    Fs, y = read('setPer.wav')
+    sd.play(y, Fs)
+    sd.wait()
+    plotWaveform(y, time, Fs)
+    return Fs, y
 
 
-setPer(1, 200, "periodfiles/a2Piano.wav", 1, 48000)
+setPer(1, 30, "periodfiles/a2Piano.wav", 1)
