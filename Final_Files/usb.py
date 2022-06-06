bbb04194be9fe92d9a2685f93b48dc4a5bbafcbc
ComKhan <conrad2756@gmail.com>
2022-06-06 11:29:49 -0700

    small change in file name

--- Final_Files/usb.py
+++ Final_Files/usb.py
@@ -6,7 +6,7 @@ from datetime import datetime
 Fs = 4800
 
 def storeUSB():
-    Fs, y = wavfile.read('/home/pi/Documents/SeniorProj-main/Final_Files/recording.wav')
+    Fs, y = wavfile.read('playback.wav')
     dt = datetime.now()
     dt_string = dt.strftime("recording%d_%m_%Y_%H_%M")
     fileName = "/media/pi/GRMCPRXFRER/" + dt_string
